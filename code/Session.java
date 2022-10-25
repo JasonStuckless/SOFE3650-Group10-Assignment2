3661c3d443ff9d00040977f3a7d550664a8da20e
Jacky Chuk Han Lee <94409754+Jacky-Lee113@users.noreply.github.com>
2022-10-25 15:10:40 -0400

    Update Session.java

--- code/Session.java
+++ code/Session.java
@@ -3,15 +3,18 @@ import java.util.ArrayList;
 public class Session {
     double calculate = 0;
 	ArrayList<String> checkout = new ArrayList<String>();
-	public void addProduct(String productName, float productPrice ) {
-		String concatProduct = productName + productPrice;
-	    checkout.add(concatProduct);
+	ArrayList<Double> price = new ArrayList<Double>();
+	public void addProduct(String productName, double productPrice ) {
+	    checkout.add(productName);
+	    price.add(productPrice);
 	    calculate += productPrice;
 	}
 	public void remove()	{
-		double val = checkout.size();
-		checkout.remove(val);
-		calculate -= val;
+		int size = checkout.size();
+		checkout.remove(size);
+		int last = price.size();
+		double rm = price.get(last);
+		calculate -= rm;
 	}
 	public double total()	{
 		return calculate;
